d519909b0c6d6844d15ec45758f6950854fb6b6a
DiscreteTom <discrete_tom@outlook.com>
2023-11-15 17:12:15 +0800

    feat: add Manager

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export * from "./env";
 export * from "./esc";
+export * from "./manager";
---
+++ src/manager.ts
@@ -0,0 +1,32 @@
+import { isCmd, isPowershell, isWindows } from "./env";
+import { esc4cmd, esc4ps, esc4sh } from "./esc";
+
+/**
+ * Auto detect the current shell and platform.
+ * This is not working in the browser.
+ */
+export class Manager {
+  readonly isWindows: boolean;
+  readonly isPowershell: boolean;
+  readonly isCmd: boolean;
+
+  constructor() {
+    this.isWindows = isWindows();
+    this.isPowershell = this.isWindows ? isPowershell() : false;
+    this.isCmd = this.isWindows ? isCmd() : false;
+  }
+
+  /**
+   * Escape a string for the current shell.
+   */
+  escape(s: string): string {
+    if (this.isWindows) {
+      if (this.isPowershell) return esc4ps(s);
+      if (this.isCmd) return esc4cmd(s);
+      return esc4sh(s); // treat unknown shell as bash
+    } else {
+      // treat non-windows as bash
+      return esc4sh(s);
+    }
+  }
+}
